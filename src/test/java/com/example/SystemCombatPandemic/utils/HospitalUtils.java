5e76c022fae4a9e3a3a8db3793644093266b4440
Augusto002 <henrique.augustob27@gmail.com>
2021-09-05 23:47:18 -0300

    Implementação do JUnit 5 para test

--- src/test/java/com/example/SystemCombatPandemic/utils/HospitalUtils.java
+++ src/test/java/com/example/SystemCombatPandemic/utils/HospitalUtils.java
@@ -2,13 +2,19 @@ package com.example.SystemCombatPandemic.utils;
 
 import com.example.SystemCombatPandemic.dto.request.HospitalDTO;
 
+import java.util.Collections;
+
 public class HospitalUtils {
 
     public static HospitalDTO createFakeDTO(){
-        return HospitalDTO.builder()
-                .cnpj("")
-                .id(1l)
-
+        HospitalDTO adas = HospitalDTO.builder()
+                .cnpj("123456789123456")
+                .endereco("adas")
+             //  .localizacao(Collections.singletonList(HospitalUtils.createFakeDTO()))
+                .nome("")
+             //   .recursos("")
+                .percentual(5)
                 .build();
+        return adas;
     }
 }
